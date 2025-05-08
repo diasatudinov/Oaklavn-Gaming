0c8a98a78b23a7d44ef2d8afbe3778836b5c0617
Dias <dias.atudinov@gmail.com>
2025-05-08 21:20:57 +0500

    app initial setup

---
+++ Oaklavn Gaming/Oaklavn Gaming/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Oaklavn Gaming
+//
+//  Created by Dias Atudinov on 08.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Oaklavn Gaming/Oaklavn Gaming/Oaklavn_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oaklavn_GamingApp.swift
+//  Oaklavn Gaming
+//
+//  Created by Dias Atudinov on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Oaklavn_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
